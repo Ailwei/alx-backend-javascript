11a3e50d713b1f5fc4e5157a09c1d678f13dec84
Ailwei <Ailweimaemu@gmail.com>
2024-04-22 13:35:46 +0000

    updated

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,6 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-param-reassign */
+
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (const value of array) {
