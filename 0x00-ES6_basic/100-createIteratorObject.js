be85628e2ef81ec28e152f91a27c0828fdecdf72
Ailwei <Ailweimaemu@gmail.com>
2024-04-22 13:56:59 +0000

    update

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,8 @@
-export default function* createIteratorObject(report) {
-  for (const department in report.allEmployees) {
-    const employees = report.allEmployees[department];
-    for (const employee of employees) {
-      yield employee;
-    }
+export default function createIteratorObject(report) {
+  const myarray = [];
+  for (const item of Object.values(report.allEmployees)) {
+    myarray.push(...item);
   }
+
+  return myarray;
 }
