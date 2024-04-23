c22a8587f9357a15395e66a302c6864ab20ebf8f
Ailwei <Ailweimaemu@gmail.com>
2024-04-23 22:35:54 +0000

    update

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,23 +1,14 @@
 import { uploadPhoto, createUser } from './utils.js';
 
-async function asyncUploadUser() {
-    try {
-        const photoPromise = uploadPhoto();
-        const userPromise = createUser();
+export default async function asyncUploadUser() {
+  let res = {};
 
-        const [photoResponse, userResponse] = await Promise.all([photoPromise, userPromise]);
-
-        return {
-            photo: photoResponse,
-            user: userResponse
-        };
-    } catch (error) {
-        console.error("An error occurred:", error);
-        return {
-            photo: null,
-            user: null
-        };
-    }
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
+  }
+  return res;
 }
-
-export default asyncUploadUser;
