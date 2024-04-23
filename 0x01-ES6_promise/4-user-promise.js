aa01f0952d0bf3e6da4dbcbef349b14f53914733
Ailwei <Ailweimaemu@gmail.com>
2024-04-23 22:03:54 +0000

    update

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,11 +1,8 @@
-
 export default function signUpUser(firstName, lastName) {
-
-return new Promise((resolve, reject) =>  {
-	const user = {
-		firstName: firstName,
-		lastName: lastName
-	};
-	resolve(user);
-});
+  return new Promise((resolve) => {
+    resolve({
+      firstName,
+      lastName,
+    });
+  });
 }
