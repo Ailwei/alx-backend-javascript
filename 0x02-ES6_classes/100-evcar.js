cce51567fd03144e264176489e4ac0a9f44945dd
Ailwei <Ailweimaemu@gmail.com>
2024-04-25 15:41:18 +0000

    evcar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,21 @@
+import Car from './10-car.js';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(newRange) {
+    this._range = newRange;
+  }
+
+  cloneCar() {
+    const { _brand, _motor, _color } = this;
+    return new Car(_brand, _motor, _color);
+  }
+}
