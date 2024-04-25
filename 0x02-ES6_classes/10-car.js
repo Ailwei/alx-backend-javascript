c8fbb912f4b22fad3df58a8303af10f8cb0f966e
Ailwei <Ailweimaemu@gmail.com>
2024-04-25 15:34:37 +0000

    Car

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,28 +1,11 @@
 export default class Car {
-	constructor(brand, motor, color) {
-		this._brand = brand;
-		this._motor = motor;
-		this._color = color;
-	}
-	get brand() {
-		return this._brand;
-	}
-	set brand(newBrand) {
-		this._brand = newBrand;
-	}
-	get motor() {
-		return this._motor;
-	}
-	set motor(newMotor) {
-		this._motor = newMotor;
-	}
-	get color() {
-		return this._color;
-	}
-	set color(newColor) {
-		this._color = newColor;
-	}
-	cloneCar() {
-		return new Car(this._brand, this._motor, this._color);
-	}
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
 }
