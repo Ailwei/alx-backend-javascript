8d52c7f066f4888f7bf73bbc37bf40e69d3c5071
Ailwei <Ailweimaemu@gmail.com>
2024-04-29 17:00:45 +0000

    11. Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,9 @@
 export const weakMap = new WeakMap();
 
-function queryAPI(endpoint) {
-    const endpointKey = JSON.stringify(endpoint);
-    const count = weakMap.get(endpointKey) || 0;
-
-    if (count >= 5) {
-        throw new Error('Endpoint load is high');
-    }
-
-    weakMap.set(endpointKey, count + 1);
+export function queryAPI(endpoint) {
+  let count = 0;
+  if (weakMap.get(endpoint)) count = weakMap.get(endpoint);
+  weakMap.set(endpoint, count + 1);
+  if (count + 1 >= 5) throw new Error('Endpoint load is high');
 }
+
