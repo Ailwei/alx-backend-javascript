f53cb57b6dfb278a9162b9fc702ddc4ebf12f3c9
Ailwei <Ailweimaemu@gmail.com>
2024-04-29 16:35:30 +0000

    4. Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,20 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-prototype-builtins */
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  return studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      newGrades.map((grade) => {
+        if (grade.studentId === student.id) {
+          student.grade = grade.grade;
+        }
 
-export default function upateStudentGradeByCity(students, city, newGrades) {
-	return students
-	.filter(student => student.location === city)
-	.map(student => {
-		const gradeObj = newGrades.find(grade => grade.studentId  === student.id);
-		const grade = gradeObj ? gradeObj.grade : 'N/A';
-		return { ...student, grade };
-	});
+        if (!student.hasOwnProperty('grade')) {
+          student.grade = 'N/A';
+        }
+        return student;
+      });
+
+      return student;
+    });
 }
