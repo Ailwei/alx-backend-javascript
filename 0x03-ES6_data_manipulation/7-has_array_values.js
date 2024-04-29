5820856803c8c0baf57dd3502b51caab190778e8
Ailwei <Ailweimaemu@gmail.com>
2024-04-29 16:45:43 +0000

    7. More set data structure

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,11 @@
-export default function(set, array) {
-	return array.every(element => set.has(element));
+/* eslint-disable array-callback-return */
+export default function hasValuesFromArray(givenSet, array) {
+  let verdict = true;
+  array.map((element) => {
+    if (!givenSet.has(element)) {
+      verdict = false;
+    }
+  });
+
+  return verdict;
 }
