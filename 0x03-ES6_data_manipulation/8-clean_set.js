f6ace63c54541b319f26960b5763a973e9ab9efc
Ailwei <Ailweimaemu@gmail.com>
2024-04-29 16:53:07 +0000

    8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,10 @@
 /* eslint-disable array-callback-return */
-export default function hasValuesFromArray(givenSet, array) {
-  let result = true;
-  array.map((element) => {
-    if (!givenSet.has(element)) {
-      result = false;
-    }
-  });
-
-  return result;
+export default function cleanSet(givenSet, startString) {
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
+  return [...givenSet]
+    .filter((str) => (str !== undefined ? str.startsWith(startString) : ''))
+    .map((str) => (str !== undefined ? str.slice(startString.length) : ''))
+    .join('-');
 }
