60208e4db58af5996ec959e9aee5764851c5cda2
Ailwei <Ailweimaemu@gmail.com>
2024-06-18 07:58:36 -0700

    5. Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,39 +1,73 @@
 const http = require('http');
-const { readFile } = require('fs').promises;
+const { readFile } = require('fs');
 
-const host = '127.0.0.1';
+const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer(async (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-
-    if (req.url === '/') {
-        res.end('Hello Holberton School!\n');
-    } else if (req.url === '/students') {
-        try {
-            const data = await readFile('database.csv', 'utf-8');
-            const students = processData(data);
-            res.end(`This is the list of our students\n${students}`);
-        } catch (error) {
-            res.end('Cannot load the database\n');
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, 'utf-8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.split('\n');
+        for (let i = 0; i < lines.length; i++) {
+          if (lines[i]) {
+            length++;
+            const field = lines[i].split(',');
+            if (students[field[3]]) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (fields[field[3]]) {
+              fields[field[3]]++;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
         }
-    } else {
-        res.end('Endpoint not found\n');
-    }
-});
-
-function processData(data) {
-    const lines = data.trim().split('\n');
-    const students = lines.map(line => {
-        const [firstname, lastname, age, field] = line.split(',');
-        return `${firstname}`;
-    }).join(', ');
-    return students;
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output.trim());
+      }
+    });
+  });
 }
 
-app.listen(port, host, () => {
-    console.log(`Server running at http://${host}:${port}/`);
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(process.argv[2])
+      .then((output) => {
+        res.end(`This is the list of our students\n${output}`);
+      })
+      .catch(() => {
+        res.statusCode = 404;
+        res.end('Cannot load the database');
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Endpoint not found');
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
