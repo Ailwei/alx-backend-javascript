b545115aa62e8d3cbeac9e82803139f218d575fd
Ailwei <Ailweimaemu@gmail.com>
2024-06-20 13:59:27 +0000

    2. Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,28 +1,28 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
-describe("Tests advanced calculateNumber function:", function(){
-    describe("Tests SUM function:", function(){
-        it("Returns 6.", function(){
-            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6)
-        })
-    })
+describe("Tests advanced calculateNumber function with Chai:", function() {
+    describe("Tests SUM function:", function() {
+        it("Returns 6.", function() {
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+        });
+    });
 
-    describe("Tests SUBTRACT function:", function(){
-        it("Returns -4.", function(){
-            assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4)
-        })
-    })
+    describe("Tests SUBTRACT function:", function() {
+        it("Returns -4.", function() {
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+        });
+    });
 
-    describe("Tests DIVIDE function:", function(){
-        it("Returns 0.2.", function(){
-            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2)
-        })
-    })
+    describe("Tests DIVIDE function:", function() {
+        it("Returns 0.2.", function() {
+            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+        });
+    });
 
-    describe("Tests DIVIDE function with divisor 0:", function(){
-        it("Returns 'Error'.", function(){
-            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error')
-        })
-    })
-}); 
+    describe("Tests DIVIDE function with divisor 0:", function() {
+        it("Returns 'Error'.", function() {
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+        });
+    });
+});
