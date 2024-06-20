94e63250b59d2d8acccb77e0775f076aed1a96bd
Ailwei <Ailweimaemu@gmail.com>
2024-06-20 13:22:56 +0000

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,28 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-  it('should return the sum of rounded numbers', function() {
-    assert.strictEqual(calculateNumber(1.4, 4.5), 6);
-    assert.strictEqual(calculateNumber(1.6, 2.3), 4);
-    assert.strictEqual(calculateNumber(-1.4, -2.5), -4);
-    assert.strictEqual(calculateNumber(0, 0), 0);
-  });
+describe("calculateNumber function", function() {
+    it('Returns 4 for two integers (1, 3).', function() {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+
+    it('Returns 5 for one float and one integer (1, 3.7).', function() {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+
+    it('Returns 4 for one float and one integer (1, 3.3).', function() {
+        assert.strictEqual(calculateNumber(1, 3.3), 4);
+    });
+
+    it('Returns 5 for one float and one integer reversed (3.7, 1).', function () {
+        assert.strictEqual(calculateNumber(3.7, 1), 5);
+    });
+
+    it('Returns 5 for two floats (1.2, 3.7).', function() {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
+    it('Returns 6 for two borderline floats (1.5, 3.7).', function() {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
 });
