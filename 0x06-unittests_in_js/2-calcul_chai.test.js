4f823659fe820179c547973fe55a750ba34713a7
Ailwei <Ailweimaemu@gmail.com>
2024-06-20 13:51:20 +0000

    2. Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
 describe("Tests advanced calculateNumber function:", function(){
     describe("Tests SUM function:", function(){
